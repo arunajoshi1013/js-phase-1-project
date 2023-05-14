9a1934cf39cf767618b5268f68a6f4636c77b1fd
Aruna  Joshi <arunajoshi1013@gmail.com>
2023-05-14 00:18:09 -0700

    js: Add wordFilter function
    This is our main function, that attaches an event listener against
    the submit button.

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 /* Global var to store the fetched data */
 let fetchedData;
 
+function wordFilter(data) {
+	/* Populate the global fetchedData var */
+	fetchedData = data;
+
+	/* First Event listener that acts on a submit event */
+	const submitForm = document.getElementById("tagform");
+	submitForm.addEventListener("submit", showChosenWords);
+}
+
 /*
  * It all begins here. Try to fetch words from the URL below.
  * If that fails, for some reason, then try the JSON server.
